bc626371828bbb057cf189a0148ccddadbf874c5
Diogo Biazus <diogo@biazus.me>
2019-08-08 10:08:32 -0400

    Install hlint after database setup

--- .circleci/setupenv.sh
+++ .circleci/setupenv.sh
@@ -2,11 +2,11 @@
 apt-get update
 apt-get install -y postgresql-client
 
-stack install --system-ghc --resolver=lts-13.29 hlint
-
 DBNAME=${DBNAME=coinberry_test}
 DBHOST=${DBHOST=localhost}
 DBUSER=${DBUSER=coinberry}
 DBURI="postgres://${DBUSER}@${DBHOST}/${DBNAME}"
 
 psql ${DBURI} -c '\set ON_ERROR_STOP' -a < postgresql-migrations/schema.sql
+
+stack install --system-ghc --resolver=lts-13.29 hlint
