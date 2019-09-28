cfc02053d18b08b983acb8e03c700dd6c7a24c6d
Diogo Biazus <diogo@biazus.me>
2019-09-28 14:18:05 -0400

    Start building only depencies and cache that

--- .circleci/setupenv.sh
+++ .circleci/setupenv.sh
@@ -8,5 +8,3 @@ DBUSER=${DBUSER=notary}
 DBURI="postgres://${DBUSER}@${DBHOST}/${DBNAME}"
 
 psql ${DBURI} -c '\set ON_ERROR_STOP' -a < postgresql-migrations/schema.sql
-
-stack install --system-ghc --resolver=lts-14.7 hlint
