6c9769ff18bf17c1108094f0db1b7a75beb89df8
Diogo Biazus <diogo@biazus.me>
2019-10-02 23:43:00 -0400

    Add signup database function bindings so we can store public keys and generate confirmation tokens

--- src/Notary/Database.hs
+++ src/Notary/Database.hs
@@ -1,5 +1,6 @@
 module Notary.Database 
     ( salt
+    , signup
 
     -- re-exports
     , Pool
@@ -18,6 +19,8 @@ import qualified Hasql.Decoders as HD
 import Hasql.Pool (Pool, UsageError, acquire, release, use)
 import Data.Either.Combinators (mapLeft)
 import Data.Vector hiding (sequence)
+import qualified Data.Aeson as JSON
+import Data.Functor.Contravariant ((>$<))
 
 salt :: MonadIO m => Pool -> Text -> m (Either ApiError ByteString)
 salt pool address = liftIO mapError
@@ -28,3 +31,15 @@ salt pool address = liftIO mapError
     sql = "SELECT notary.salt($1)"
     encoder = HE.param (HE.nonNullable HE.text)
     decoder = HD.singleRow (HD.column (HD.nonNullable HD.bytea))
+
+signup :: MonadIO m => Pool -> Text -> JSON.Value -> m (Either ApiError Text)
+signup pool address key = liftIO mapError
+  where
+    mapError = mapLeft (\_ -> Error "Database Error") <$> use pool (statement (address, key) selectSignup)
+    selectSignup :: Statement (Text, JSON.Value) Text
+    selectSignup = Statement sql encoder decoder True
+    sql = "SELECT notary.signup($1, $2)"
+    encoder = 
+      (fst >$< HE.param (HE.nonNullable HE.text)) <> 
+      (snd >$< HE.param (HE.nonNullable HE.jsonb))
+    decoder = HD.singleRow (HD.column (HD.nonNullable HD.text))
--- test/DatabaseSpec.hs
+++ test/DatabaseSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE QuasiQuotes #-}
 module DatabaseSpec (spec) where
 
 import Protolude hiding (get)
@@ -5,15 +6,20 @@ import Protolude hiding (get)
 import Notary
 import Notary.Database
 import Test.Hspec
+import Data.Aeson.QQ
 
 withDatabaseConnection :: IO Pool
 withDatabaseConnection = acquire (10, 10, "postgres://notary_public:test@localhost/notary_test")
 
 spec :: Spec
 spec =
-  before withDatabaseConnection $
+  before withDatabaseConnection $ do
     describe "salt" $
       it "should be successful and idenpotent" $ \p -> do
         previousSalt <- salt p "foo@bar.com"
         previousSalt `shouldSatisfy` isRight
         salt p "foo@bar.com" `shouldReturn` previousSalt
+    describe "signup" $
+      it "should be successful" $ \p -> do
+        token <- signup p "foo@bar.com" [aesonQQ| { } |]
+        token `shouldSatisfy` isRight
